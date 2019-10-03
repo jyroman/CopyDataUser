b71fef9888b7f06ccab9ba09fa0fe935620e8421
jyroman <jyroman@hotmail.fr>
2019-10-03 12:00:30 +0200

    Added new funtions
    Added :
    - Log Transfer on the destination computer
    - Recommended number of Threads
    - Added new infomations in the log file
    - Added resumable file transfer

--- CopyDataUser.bat
+++ CopyDataUser.bat
@@ -3,7 +3,7 @@
 ::International charcters
 chcp 858
 echo.
-echo Profile data copy script V1.8
+echo Profile data copy script V1.9A
 echo.
 echo /!\ This script must be executed on the source computer, abort if this is not the right computer /!\
 echo.
@@ -82,7 +82,7 @@ if exist "Z:\users\%user%" (
 
 :: Copy arguments
 :CopArg
-set /p NbC= Enter the number of cores : 
+set /p NbC= Enter the number of threads (paralel file copy 16 is recommended - 8 if not sure) : 
 set /p LogF= Enter the name of the log file : 
 echo Number of cores : %NbC% - Name of the log file : %LogF%
 :CopArgVal
@@ -94,55 +94,64 @@ if %InfoOK% == Y (
 	) else ( 
 		goto CopArgVal
 	)
-echo The log file will be located at C:\users\%user%\%LogF%.log
+echo The log file will be located in C:\users\%user%\ on the destination computer
 echo.
 pause
 echo.
+
+::Writing informations into the log file
+
+echo Source computer : %COMPUTERNAME% >> %LogF%.log
+echo Destination computer : %CompName% >> %LogF%.log
+echo User data to copy : %user% >> %LogF%.log
+echo Admin account used : %userA% >> %LogF%.log
+echo.
+
 
 :: Begining data copy 
 echo Beginning Data copy
 echo.
 echo Profile copy : Desktop 
-robocopy /E /XC /XN /XO /TEE /ETA /MT:%NbC% /log+:%LogF%.log C:\users\%user%\Desktop Z:\users\%user%\Desktop
+robocopy /ZB /V /E /XC /XN /XO /TEE /ETA /MT:%NbC% /log+:%LogF%.log C:\users\%user%\Desktop Z:\users\%user%\Desktop
 
 echo Profile copy : Contacts 
-robocopy /E /XC /XN /XO /TEE /ETA /MT:%NbC% /log+:%LogF%.log C:\users\%user%\Contacts Z:\users\%user%\Contacts
+robocopy /ZB /V /E /XC /XN /XO /TEE /ETA /MT:%NbC% /log+:%LogF%.log C:\users\%user%\Contacts Z:\users\%user%\Contacts
 
 echo Profile copy : Documents 
-robocopy /E /XC /XN /XO /TEE /ETA /MT:%NbC% /log+:%LogF%.log C:\users\%user%\Documents Z:\users\%user%\Documents /XD "My Music" "My Pictures" "My Videos" "Ma Musique" "Mes Images" "Mes Vid‚os"
+robocopy /ZB /V /E /XC /XN /XO /TEE /ETA /MT:%NbC% /log+:%LogF%.log C:\users\%user%\Documents Z:\users\%user%\Documents /XD "My Music" "My Pictures" "My Videos" "Ma Musique" "Mes Images" "Mes Vid‚os"
 
 echo Profile copy : Downloads 
-robocopy /E /XC /XN /XO /TEE /ETA /MT:%NbC% /log+:%LogF%.log C:\users\%user%\Downloads Z:\users\%user%\Downloads
+robocopy /ZB /V /E /XC /XN /XO /TEE /ETA /MT:%NbC% /log+:%LogF%.log C:\users\%user%\Downloads Z:\users\%user%\Downloads
 
 echo Profile copy : Favorites (Internet Explorer)
-robocopy /E /XC /XN /XO /TEE /ETA /MT:%NbC% /log+:%LogF%.log C:\users\%user%\Favorites Z:\users\%user%\Favorites
+robocopy /ZB /V /E /XC /XN /XO /TEE /ETA /MT:%NbC% /log+:%LogF%.log C:\users\%user%\Favorites Z:\users\%user%\Favorites
 
 echo Profile copy : Links 
-robocopy /E /XC /XN /XO /TEE /ETA /MT:%NbC% /log+:%LogF%.log C:\users\%user%\Links Z:\users\%user%\Links
+robocopy /ZB /V /E /XC /XN /XO /TEE /ETA /MT:%NbC% /log+:%LogF%.log C:\users\%user%\Links Z:\users\%user%\Links
 
 echo Profile copy : Music 
-robocopy /E /XC /XN /XO /TEE /ETA /MT:%NbC% /log+:%LogF%.log C:\users\%user%\Music Z:\users\%user%\Music
+robocopy /ZB /V /E /XC /XN /XO /TEE /ETA /MT:%NbC% /log+:%LogF%.log C:\users\%user%\Music Z:\users\%user%\Music
 
 echo Profile copy : Pictures 
-robocopy /E /XC /XN /XO /TEE /ETA /MT:%NbC% /log+:%LogF%.log C:\users\%user%\Pictures Z:\users\%user%\Pictures
+robocopy /ZB /V /E /XC /XN /XO /TEE /ETA /MT:%NbC% /log+:%LogF%.log C:\users\%user%\Pictures Z:\users\%user%\Pictures
 
 echo Profile copy : Videos
-robocopy /E /XC /XN /XO /TEE /ETA /MT:%NbC% /log+:%LogF%.log C:\users\%user%\Videos Z:\users\%user%\Videos
+robocopy /ZB /V /E /XC /XN /XO /TEE /ETA /MT:%NbC% /log+:%LogF%.log C:\users\%user%\Videos Z:\users\%user%\Videos
 
 echo Profile copy : Outlook Signature
 if not exist Z:\Users\%user%\AppData\Roaming\Microsoft\Signatures (mkdir Z:\Users\%user%\AppData\Roaming\Microsoft\Signatures)
 
-robocopy /E /IS /TEE /ETA /MT:%NbC% /log+:%LogF%.log C:\Users\%user%\AppData\Roaming\Microsoft\Signatures Z:\users\%user%\AppData\Roaming\Microsoft\Signatures
+robocopy /ZB /V /E /IS /TEE /ETA /MT:%NbC% /log+:%LogF%.log C:\Users\%user%\AppData\Roaming\Microsoft\Signatures Z:\users\%user%\AppData\Roaming\Microsoft\Signatures
 
 echo Profile copy : Chrome bookmarks
 
-robocopy /IS /TEE /ETA /MT:%NbC% /log+:%LogF%.log "C:\Users\%user%\AppData\Local\Google\Chrome\User Data\Default" "Z:\Users\%user%\AppData\Local\Google\Chrome\User Data\Default" Bookmarks
-
-echo Log Transfer
-robocopy robocopy /IS /TEE /ETA /MT:%NbC% %~dp0 "Z:\Users\%user%\" %LogF%.log
+robocopy /ZB /V /IS /TEE /ETA /MT:%NbC% /log+:%LogF%.log "C:\Users\%user%\AppData\Local\Google\Chrome\User Data\Default" "Z:\Users\%user%\AppData\Local\Google\Chrome\User Data\Default" Bookmarks
 
 echo.
-echo End of the data copy, if there are any files outside of the profile data, you must copy them manually.
+echo End of the data copy, if there are any files outside of the profile data, you must copy them manually. >> %LogF%.log
 echo.
+
+::Tranfer of log to the destination computer
+robocopy /IS /TEE /ETA /MT:%NbC% %~dp0. "Z:\Users\%user%\" %LogF%.log
 
 pause
